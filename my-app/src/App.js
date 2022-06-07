ee1bf3f06dc1c3d49969031f5ccc553652fff4ed
Damaris Palacios <dslpz728@gmail.com>
2022-06-06 20:22:58 -0500

    adds location

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import { FaBeer } from 'react-icons/fa';
+// import { FaBeer } from 'react-icons/fa';
 import Inputs from './components/Inputs';
+import Location from './components/Location';
 
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md bg-gradient-to-br from-cyan-900 to-cyan-200 h-fit shadow-xl shadow-gray-600 mt-4 py-5 px-32">
       <Inputs />
+
+      
+      <Location />
     </div>
   );
 }
---
+++ my-app/src/components/Location.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Location = () => {
+  return (
+    <div>
+        <div className='flex justify-center my-6 items-center'>
+            <p className='text-white text-xl font-extralight'>
+                    Monday, 6 June 2022  |  Local time: 7:54 PM
+            </p>
+        </div>
+
+        <div className='my-3 items-center flex justify-center'>
+            <p className='text-white text-3xl font-medium'>Houston, Texas</p>
+        </div>
+    </div>
+  )
+}
+
+export default Location
