b42964dc0f68acbeac165710c9a1ec34765eefbe
Damaris Palacios <dslpz728@gmail.com>
2022-06-06 19:42:35 -0500

    adds component file and search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
+import Inputs from './components/Inputs';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1 className= 'text-2xl font-bold text:red text-red-500'>Hello World</h1>
+    <div className="mx-auto max-w-screen-md bg-gradient-to-br from-cyan-900 to-cyan-200 h-fit shadow-xl shadow-gray-600 mt-4 py-5 px-32">
+      <Inputs />
     </div>
   );
 }
---
+++ my-app/src/components/Inputs.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {FaLocationArrow, FaSearch} from "react-icons/fa"
+
+
+
+const Inputs = () => {
+  return (
+    <div className="flex flex-row justify-center my-6">
+        <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
+
+            <input 
+            type='text' 
+            className='text-xl font-light focus:outline-none p-2 w-full shadow-xl capitalize placeholder:lowercase' placeholder='Search City'/>
+        <FaSearch size={30} className='text-white transition ease-out hover:scale-125'/>
+        <FaLocationArrow size={30} className='text-white transition ease-out hover:scale-125'/>
+        </div>
+
+        <div className='flex flex-row w-1/4 items-center justify-center'>
+            <button className='font-light text-xl text-white' name='imperial'>°F </button>
+            <p className='mx-2 text-white text-xl'> |</p>
+            <button className='font-light text-xl text-white' name='metric'>°C </button>
+        </div>
+    </div>
+  )
+}
+
+export default Inputs
